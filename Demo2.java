b988da3f5c5aaaae14c95581ca62521ae194a980
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-14 12:00:35 +0530

    third

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Demo2 {
     public static void main(String[] args) {
+
+        Deque<Integer> myDequeDeque = new ArrayDeque<>();
+        myDequeDeque.push(10); 
+        myDequeDeque.offer(20);
+        myDequeDeque.offer(30);
+        myDequeDeque.push(40);
+        myDequeDeque.offer(50);
+        myDequeDeque.offer(60);
+        System.out.println(myDequeDeque);
+        myDequeDeque.pop(); //poll first
+        myDequeDeque.poll();//poll first
+        System.out.println(myDequeDeque);
+        
         
     }
 }
